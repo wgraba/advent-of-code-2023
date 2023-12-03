d9e7549d46bdd31e23b2c76bbec9068d429d048c
Wesley Graba <wesley.graba@gmail.com>
2023-12-03 05:40:17 +0000

    Day 2b solution

--- src/main.rs
+++ src/main.rs
@@ -11,7 +11,7 @@ struct Round {
 }
 
 struct Game {
-    id: u32,
+    _id: u32,
     rounds: Vec<Round>,
 }
 
@@ -25,47 +25,30 @@ struct Args {
 fn main() {
     let cli_args = Args::parse();
 
-    let max_round = Round {
-        num_blue: 14,
-        num_green: 13,
-        num_red: 12,
-    };
-
     let file_input = File::open(cli_args.input.as_str())
         .expect(format!("Could not open file {}", cli_args.input).as_str());
     let file_reader = BufReader::new(file_input);
 
     let mut file_lines = file_reader.lines();
 
-    let mut possible_game_total: u32 = 0;
+    let mut total_game_powers: u32 = 0;
     while let Some(Ok(line)) = file_lines.next() {
         let game = parse_game_input(&line);
-        let mut is_possible = false;
-        for round in game.rounds {
-            is_possible = round.num_blue <= max_round.num_blue
-                && round.num_green <= max_round.num_green
-                && round.num_red <= max_round.num_red;
-            if !is_possible {
-                break;
-            }
-        }
+        let max_possible_round = game.rounds.into_iter().reduce(|acc, r| Round {
+            num_red: std::cmp::max(acc.num_red, r.num_red),
+            num_green: std::cmp::max(acc.num_green, r.num_green),
+            num_blue: std::cmp::max(acc.num_blue, r.num_blue),
+        });
 
-        if is_possible {
-            possible_game_total += game.id;
-        }
-
-        println!(
-            "Game {0:04}: {1}",
-            game.id,
-            if is_possible {
-                "Possible"
-            } else {
-                "Impossible"
-            }
-        );
+        let power = if let Some(round) = max_possible_round {
+            round.num_blue * round.num_green * round.num_red
+        } else {
+            0
+        };
+        total_game_powers += power;
     }
 
-    println!("Total Possible Game IDs: {possible_game_total}");
+    println!("Total Game Powers: {total_game_powers}");
 }
 
 fn parse_game_input(input: &str) -> Game {
@@ -105,7 +88,7 @@ fn parse_game_input(input: &str) -> Game {
         });
     }
     Game {
-        id: game_id,
+        _id: game_id,
         rounds: rounds,
     }
 }
