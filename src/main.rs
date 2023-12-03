3c7d61b1c8c7af27687e72b2fa6f16688c5fe814
Wesley Graba <wesley.graba@gmail.com>
2023-12-03 05:23:35 +0000

    Day 2a solution

--- src/main.rs
+++ src/main.rs
@@ -4,27 +4,32 @@ use std::{
     io::{BufRead, BufReader},
 };
 
+struct Round {
+    num_red: u32,
+    num_green: u32,
+    num_blue: u32,
+}
+
+struct Game {
+    id: u32,
+    rounds: Vec<Round>,
+}
+
 /// Simple program to greet a person
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None)]
 struct Args {
-    /// Name of the person to greet
+    /// Path to puzzle input
     input: String,
 }
 fn main() {
     let cli_args = Args::parse();
 
-    let words_to_digits = vec![
-            ("one", "o1e"),
-            ("two", "t2o"),
-            ("three", "t3e"),
-            ("four", "f4r"),
-            ("five", "f5e"),
-            ("six", "s6x"),
-            ("seven", "s7n"),
-            ("eight", "e8t"),
-            ("nine", "n9e"),
-        ];
+    let max_round = Round {
+        num_blue: 14,
+        num_green: 13,
+        num_red: 12,
+    };
 
     let file_input = File::open(cli_args.input.as_str())
         .expect(format!("Could not open file {}", cli_args.input).as_str());
@@ -32,37 +37,75 @@ fn main() {
 
     let mut file_lines = file_reader.lines();
 
-    let mut total: u32 = 0;
-    let mut line_num = 1;
-    while let Some(Ok(mut line)) = file_lines.next() {
-        let input = line.clone();
-        line = line.to_ascii_lowercase();
-
-        // Find first and last digits
-        for (word, digit) in &words_to_digits {
-            line = line.replace(word, digit);
-        }
-
-        let digits: Vec<u32> = line.chars().filter_map(|c| c.to_digit(10)).collect();
-
-        let first_digit = digits.first();
-        let last_digit = digits.last();
-        match (first_digit, last_digit) {
-            (Some(first), Some(last)) => {
-                let num_str = format!("{first}{last}");
-                let num: u8 = num_str
-                    .parse()
-                    .expect(format!("Unable to convert {num_str}").as_str());
-
-                total += num as u32;
-
-                println!("{line_num}: {input} -> {digits:?} -> {num}");
-                line_num += 1;
+    let mut possible_game_total: u32 = 0;
+    while let Some(Ok(line)) = file_lines.next() {
+        let game = parse_game_input(&line);
+        let mut is_possible = false;
+        for round in game.rounds {
+            is_possible = round.num_blue <= max_round.num_blue
+                && round.num_green <= max_round.num_green
+                && round.num_red <= max_round.num_red;
+            if !is_possible {
+                break;
             }
-
-            _ => panic!("Could not find first or last ASCII digit in '{line}'"),
         }
+
+        if is_possible {
+            possible_game_total += game.id;
+        }
+
+        println!(
+            "Game {0:04}: {1}",
+            game.id,
+            if is_possible {
+                "Possible"
+            } else {
+                "Impossible"
+            }
+        );
     }
 
-    println!("File total: {total}");
+    println!("Total Possible Game IDs: {possible_game_total}");
+}
+
+fn parse_game_input(input: &str) -> Game {
+    let game_id: u32 = input
+        .split_once(":")
+        .unwrap()
+        .0
+        .split_once(" ")
+        .unwrap()
+        .1
+        .parse()
+        .expect("Game ID is not a number");
+    let rounds_str = input.split_once(":").unwrap().1.split(";");
+
+    let mut num_red: u32 = 0;
+    let mut num_blue: u32 = 0;
+    let mut num_green: u32 = 0;
+    let mut rounds: Vec<Round> = Vec::new();
+    for round in rounds_str {
+        let balls = round.split(",");
+        for ball in balls {
+            let (num_balls_str, ball_color) = ball.trim().split_once(" ").unwrap();
+            let num_balls: u32 = num_balls_str
+                .parse()
+                .expect(&format!("Game {game_id}: unable to parse number of balls."));
+            match ball_color {
+                "red" => num_red = num_balls,
+                "blue" => num_blue = num_balls,
+                "green" => num_green = num_balls,
+                _ => panic!("Unknown ball color '{ball_color}'"),
+            };
+        }
+        rounds.push(Round {
+            num_red: num_red,
+            num_blue: num_blue,
+            num_green: num_green,
+        });
+    }
+    Game {
+        id: game_id,
+        rounds: rounds,
+    }
 }
